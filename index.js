77cf11092dcbe376973c6feb1e4d22ac8357803a
Vu Anh Hao <hi@anhhao.me>
2021-06-09 09:10:46 +0700

    Basic layout

---
+++ index.js
@@ -0,0 +1,25 @@
+// Layout Components
+export { default as MDashboardLayout } from "./src/layouts/MDashboardLayout.vue";
+export { default as MAuthLayout } from "./src/layouts/MAuthLayout.vue";
+
+// Common Components
+export { default as MAlert } from "./src/components/MAlert.vue";
+export { default as MBreadcrumb } from "./src/components/MBreadcrumb.vue";
+export { default as MButton } from "./src/components/MButton.vue";
+export { default as MConfirmDialog } from "./src/components/MConfirmDialog.vue";
+export { default as MDialog } from "./src/components/MDialog.vue";
+export { default as MDropdown } from "./src/components/MDropdown.vue";
+export { default as MInput } from "./src/components/MInput.vue";
+export { default as MInputDialog } from "./src/components/MInputDialog.vue";
+export { default as MLink } from "./src/components/MLink.vue";
+export { default as MList } from "./src/components/MList.vue";
+export { default as MNotification } from './src/components/MNotification.vue';
+export { default as MPanel } from "./src/components/MPanel.vue";
+export { default as MTable } from "./src/components/MTable.vue";
+
+// Plugins
+export { default as MDbPlugin } from "./src/plugins/mdb";
+export { default as MDialogPlugin } from "./src/plugins/mdialog";
+export { default as MHttpPlugin } from "./src/plugins/mhttp";
+export { default as MNotificationPlugin } from "./src/plugins/mnoti";
+export { default as MUtils } from "./src/plugins/mutils";
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from "url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/vue-minimal-admin/",
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => tag.startsWith("ion-"),
+        },
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
