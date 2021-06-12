097323bf94505662e60ed8bd2f75d0f4d08052d7
Vu Anh Hao <hi@anhhao.me>
2021-06-12 09:12:13 +0700

    Add MBreadcrumb

---
+++ src/components/MBreadcrumb.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink } from "vue-router";
+defineProps(["items"]);
+</script>
+
+<template>
+  <div class="inline-flex items-center space-x-2">
+    <RouterLink
+      class="text-sm inline-flex items-center mactive:text-stone-400 dark:mactive:text-stone-500"
+      :to="item.href"
+      v-for="(item, index) in items"
+      :active="item.active || null"
+    >
+      <ion-icon
+        class="mr-2"
+        :name="index === 0 ? 'home' : 'chevron-forward'"
+      ></ion-icon>
+      <span>{{ item.text }}</span>
+    </RouterLink>
+  </div>
+</template>
