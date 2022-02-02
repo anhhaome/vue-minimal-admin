58ec640a9d8a1627993d447560bbd36785e353b6
Vu Anh Hao <hi@anhhao.me>
2022-02-02 09:16:08 +0700

    Add Input

---
+++ src/components/MInput.vue
@@ -0,0 +1,27 @@
+<script setup>
+import uniqid from "uniqid";
+
+defineProps(["label", "type", "modelValue"]);
+defineEmits(["update:modelValue"]);
+
+const id = uniqid();
+</script>
+
+<template>
+  <div class="relative my-6">
+    <input
+      :type="type"
+      :id="`minput-${id}`"
+      :placeholder="label"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      class="block border w-full p-3 rounded-lg peer outline-none focus:border-black dark:bg-gray-900 dark:border-gray-500 dark:focus:border-primary-500"
+    />
+    <label
+      :for="`minput-${id}`"
+      class="absolute top-[-.7em] left-2 px-1 bg-white cursor-text transition-all text-[0.7rem] text-gray-600 peer-focus:top-[-.7em] peer-focus:text-[0.7rem] peer-focus:text-black peer-placeholder-shown:top-[1em] peer-placeholder-shown:text-[1em] dark:bg-gray-900 dark:text-gray-500 dark:peer-focus:text-primary-500"
+    >
+      {{ label }}
+    </label>
+  </div>
+</template>
---
+++ src/components/MInputDialog.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref, watch } from "vue";
+
+import MDialog from "./MDialog.vue";
+import MInput from "./MInput.vue";
+import MButton from "./MButton.vue";
+
+const dialog = ref(null);
+const result = ref(undefined);
+const inputValue = ref("");
+
+defineExpose({
+  show() {
+    return new Promise((resolve) => {
+      dialog.value.show();
+
+      const unwatch = watch(result, (newValue) => {
+        unwatch();
+        dialog.value.hide();
+        resolve(newValue ? inputValue.value : null);
+      });
+    });
+  },
+});
+</script>
+
+<template>
+  <MDialog ref="dialog" :label="false" @close="result = null">
+    <div class="text-center">
+      <MInput label="Enter your input" v-model="inputValue" />
+      <MButton variant="primary" @click="result = true">OK</MButton>
+    </div>
+  </MDialog>
+</template>
