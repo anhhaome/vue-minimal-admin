0b7f910a3ef7101555d22a56de99ffe5742c11c5
Vu Anh Hao <hi@anhhao.me>
2022-02-03 09:17:13 +0700

    Add MList

---
+++ src/components/MList.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps(["items"]);
+</script>
+
+<template>
+  <ul
+    class="w-48 bg-white border rounded dark:bg-gray-800 dark:border-gray-700"
+    v-if="props.items"
+  >
+    <li
+      class="py-2 px-4 cursor-pointer first:rounded-t last:rounded-b hover:bg-gray-100 dark:hover:bg-gray-700"
+      v-for="item in props.items"
+    >
+      {{ item }}
+    </li>
+  </ul>
+</template>
