d2cd5914dd9eadf22ef927467fa8697de907d314
Vu Anh Hao <hi@anhhao.me>
2022-02-03 09:16:59 +0700

    Add MLink

---
+++ src/components/MLink.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink } from "vue-router";
+
+defineProps(["to"]);
+defineEmits(["click"]);
+</script>
+
+<template>
+  <RouterLink
+    :to="to"
+    class="text-primary-500 font-medium hover:underline cursor-pointer"
+    v-if="to"
+  >
+    <slot></slot>
+  </RouterLink>
+  <a
+    class="text-primary-500 font-medium hover:underline cursor-pointer"
+    v-if="!to"
+    @click="$emit('click')"
+  >
+    <slot></slot>
+  </a>
+</template>
