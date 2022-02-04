d469fb6c59eba9d28ec87d685b35d8c042c1d45a
Vu Anh Hao <hi@anhhao.me>
2022-02-04 09:17:56 +0700

    Add MTable

---
+++ src/components/MTable.vue
@@ -0,0 +1,95 @@
+<script setup>
+import { ref, watch } from 'vue';
+
+const props = defineProps(['data', 'labels']);
+const localLables = ref([]);
+
+const reloadLabels = () => {
+  if (props.labels)
+    return localLables.value = props.labels;
+
+  const s = new Set();
+  for (let item of props.data) {
+    for (let name in item) {
+      s.add(name);
+    }
+  }
+
+  localLables.value = Array.from(s);
+}
+
+const formatLabel = label => {
+  return label.split(/(?=[A-Z])/).map(w => w.length > 0 ? (w[0].toUpperCase() + w.substr(1)) : '').join(' ');
+}
+
+const hasSlot = (name, slots) => {
+  for (let slot in slots){
+    if (slot.indexOf(name) === 0)
+      return true;
+  }
+
+  return false;
+}
+
+watch(() => props.data, reloadLabels);
+watch(() => props.labels, reloadLabels)
+
+reloadLabels();
+
+</script>
+
+<template>
+  <table class="table-fixed w-full">
+    <thead class="bg-gray-50 dark:bg-gray-800 border-b
+    dark:border-gray-700">
+      <th class="py-2 px-4 font-normal text-xs text-left uppercase" v-for="label in localLables">
+        <slot :name="`head(${label})`" v-bind="{label}">
+          <slot :name="`head()`" v-bind="{label}">
+            {{ formatLabel(label) }}
+          </slot>
+        </slot>
+      </th>
+    </thead>
+    <tbody>
+      <!-- top cell -->
+      <tr class="border-b
+        dark:border-gray-700" v-if="hasSlot('topcell', $slots)">
+        <td class="py-2 px-4"
+          v-for="label in localLables"
+        >
+          <slot :name="`topcell(${label})`" v-bind="{label}">
+            <slot :name="`topcell()`" v-bind="{label}"></slot>
+          </slot>
+        </td>
+      </tr>
+
+      <!-- body cell -->
+      <tr class="border-b
+        dark:border-gray-700"
+        v-for="row of props.data"
+      >
+        <td class="py-2 px-4"
+          v-for="label in localLables"
+        >
+          <slot :name="`cell(${label})`" v-bind="{label, value: row[label]}">
+            <slot :name="`cell()`" v-bind="{label, value: row[label]}">
+              {{ row[label] }}
+            </slot>
+          </slot>
+        </td>
+      </tr>
+
+      <!-- bottom cell -->
+      <tr class="border-b
+        dark:border-gray-700" v-if="hasSlot('bottomcell', $slots)">
+        <td class="py-2 px-4"
+          v-for="label in localLables"
+        >
+          <slot :name="`bottomcell(${label})`" v-bind="{label}">
+            <slot :name="`bottomcell()`" v-bind="{label}"></slot>
+          </slot>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
