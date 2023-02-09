18836d8ae59279e1ddc31f7b17c83662ebc1cecb
Vu Anh Hao <hi@haova.me>
2023-02-09 20:37:47 +0700

    Add deploy

--- example/vite.config.js
+++ example/vite.config.js
@@ -1,14 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
+const base = process.env.EXAMPLE_BASE || '';
+
 export default defineConfig({
+  envPrefix: 'EXAMPLE_',
+
+  base: `/${base}`,
+
   plugins: [vue()],
+
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
