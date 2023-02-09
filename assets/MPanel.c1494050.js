f238088d67591de25a22c19e8d19de168d6dbe25
Vu Anh Hao <hi@haova.me>
2023-02-09 20:40:03 +0700

    Updates

--- assets/MPanel.c1494050.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./plugin-vue_export-helper.21dcd24c.js";import{o,c as a,h as t}from"./index.15fff4fb.js";const s={},c={class:"p-4 rounded border bg-white border-gray-100 mt-4 dark:bg-gray-900 dark:border-gray-800"};function d(r,n){return o(),a("div",c,[t(r.$slots,"default")])}var f=e(s,[["render",d]]);export{f as M};
