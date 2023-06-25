3e51d4a4d6715997bc77aab2f82cd59db0cd71c2
Vu Anh Hao <hi@haova.me>
2023-06-25 15:03:33 +0700

    export theme.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,34 +17,5 @@ export default {
     color: 'base',
     font: 'basic'
   }),
-  // theme: {
-  //   colors: {
-  //     primary: colors.cyan,
-  //     secondary: colors.stone,
-  //     danger: colors.rose,
-  //     info: colors.indigo,
-  //     warn: colors.amber,
-  //     success: colors.emerald,
-  //     ...colors
-  //   },
-  //   fontFamily: {
-  //     sans: ['PoppinsVN', 'sans-serif'],
-  //     mono: ['"Fira Code"']
-  //   },
-  //   extend: {},
-  //   fontSize: {
-  //     xs: '.75rem',
-  //     sm: '.825rem',
-  //     base: '1rem',
-  //     lg: '1.125rem',
-  //     xl: '1.25rem',
-  //     '2xl': '1.5rem',
-  //     '3xl': '1.875rem',
-  //     '4xl': '2.25rem',
-  //     '5xl': '3rem',
-  //     '6xl': '4rem',
-  //     '7xl': '5rem'
-  //   }
-  // },
   plugins: []
 }
